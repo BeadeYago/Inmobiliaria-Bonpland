9f9221bc713eedd43887953ef6776ae05a704fe8
BeadeYago <yagito2001@gmail.com>
2023-03-12 16:27:55 -0300

    Start of the project

---
+++ src/test/java/com/inmobiliria/bonpland/BondplandApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inmobiliria.bonpland;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BondplandApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
